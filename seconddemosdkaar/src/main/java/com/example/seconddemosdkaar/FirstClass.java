73e1e2c32dd2a2305ce99db758fdf2af9b755e93
manu flores <fmanuela499@gmail.com>
2019-05-27 17:23:36 -0500

    feat(project): first implementation project :art:
     - Add first implementation to an aar module.

---
+++ seconddemosdkaar/src/main/java/com/example/seconddemosdkaar/FirstClass.java
@@ -0,0 +1,11 @@
+package com.example.seconddemosdkaar;
+
+import okhttp3.OkHttpClient;
+
+public class FirstClass {
+
+    public String getOkhttpExample() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        return okHttpClient.toString();
+    }
+}
